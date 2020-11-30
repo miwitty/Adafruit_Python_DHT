cb7200f9b3125012bca6db4adae8271ffe7453ac
miwitty <witty60123@gmail.com>
2020-11-30 22:56:23 +0800

    modified:   AdafruitDHT.py

--- examples/AdafruitDHT.py
+++ examples/AdafruitDHT.py
@@ -36,6 +36,36 @@ else:
     print('Example: sudo ./Adafruit_DHT.py 2302 4 - Read from an AM2302 connected to GPIO pin #4')
     sys.exit(1)
 
+import time
+import sys
+import http.client as http
+Import urllib
+import json
+deviceId = "DH0ZnDBP“
+deviceKey = “BxBl0Lvt0cSAcAfd“ 
+def post_to_mcs(payload): 
+	headers = {"Content-type": "application/json", "deviceKey": deviceKey} 
+	not_connected = 1 
+	while (not_connected):
+		try:
+			conn = http.HTTPConnection("api.mediatek.com:80")
+			conn.connect() 
+			not_connected = 0 
+		except (http.HTTPException, socket.error) as ex: 
+			print ("Error: %s" % ex)
+ 			time.sleep(10)
+			 # sleep 10 seconds 
+	conn.request("POST", "/mcs/v2/devices/" + deviceId + "/datapoints", json.dumps(payload), headers) 
+	response = conn.getresponse() 
+	print( response.status, response.reason, json.dumps(payload), time.strftime("%c")) 
+	data = response.read() 
+	conn.close() 
+
+
+
+
+
+
 # Try to grab a sensor reading.  Use the read_retry method which will retry up
 # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
 h0, t0 = Adafruit_DHT.read_retry(sensor, pin)
@@ -51,6 +81,11 @@ while True:
 	h0, t0 = Adafruit_DHT.read_retry(sensor, pin)
 	if h0 is not None and t0 is not None:
 		print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(t0, h0))
+			payload = {"datapoints":[{"dataChnId":"Humidity","values":{"value":h0}},
+		{"dataChnId":"Temperature","values":{"value":t0}}]} 
+		post_to_mcs(payload)
+		time.sleep(10) 
+
 	else:
 		print('Failed to get reading. Try again!')
 		sys.exit(1)
